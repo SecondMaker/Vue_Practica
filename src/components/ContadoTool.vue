e9f24ddc956f12bbfb415b76793476d9564e21c1
aelis <delahoz_ep@hotmail.com>
2025-05-18 10:40:46 -0400

    Lista de tareas sencillas, con componentes reactivos

---
+++ src/components/ContadoTool.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>Hola Mundo</h1>
+  <br />
+  <p>{{ contador }}</p>
+  <br />
+  <div>
+    <button @click="Sumar">Sumar un numero</button>
+    <button @click="Restar">Restar un numero</button>
+  </div>
+</template>
+<script setup>
+import { ref } from 'vue'
+const contador = ref(0)
+const Sumar = () => {
+  contador.value++
+}
+const Restar = () => {
+  contador.value--
+}
+</script>
+
+<style scoped>
+p {
+  color: green;
+}
+</style>
